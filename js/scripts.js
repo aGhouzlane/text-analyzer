ab34bb5ffcf5f139828ff1a8a9324d5498da1a04
aGhouzlane <ahmedghouzlane@gmail.com>
2021-03-30 14:32:36 -0400

    Test additional functions
    Co-authored-by: Tori West <victoria_west@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,54 @@
 // Business Logic
 function wordCounter(text) {
-  if(text.length===0){
+  if (text.trim().length === 0) {
     return 0;
   }
   let wordCount = 0;
   const wordArray = text.split(" ");
-  wordArray.forEach(function(word) {
-    wordCount++;
+  wordArray.forEach(function (word) {
+    if (!Number(word)) {
+      wordCount++;
+    }
   });
   return wordCount;
 }
+
+
+function numberOfOccurrencesInText(word, text) {
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  wordArray.forEach(function (element) {
+    if (element.includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+const text = "red RED Red";
+const word = "red";
+
+console.log(numberOfOccurrencesInText(word, text));
+
+
+const result = numberOfOccurrencesInText(word, text);
+
+function includesRarestLetter(word) {
+  if (word.toLowerCase().includes("q")) {
+    return true;
+  }
+  return false;
+}
+//console.log(includesRarestLetter(word));
+
+//console.log("red! red. red?".includes("red"));
+
+
+
+
+
+
+
